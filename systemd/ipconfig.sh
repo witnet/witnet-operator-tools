8be53a8f1e5dcecc2209bf8d55d03824d764ba57
Bertrand Juglas <b@juglas.name>
2020-06-18 15:45:14 +0200

    take only first line
    to make it work with Digital Ocean droplets

--- systemd/ipconfig.sh
+++ systemd/ipconfig.sh
@@ -1,3 +1,3 @@
-IP=`ip address show dev eth0 | grep inet | cut -d ' ' -f 6 | cut -d '/' -f 1`
+IP=`ip address show dev eth0 | grep inet | head -n1 | cut -d ' ' -f 6 | cut -d '/' -f 1`
 echo $IP
 sed -i "s/0.0.0.0:21337/$IP:21337/" witnet.toml
