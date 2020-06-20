38101b259a494b0ed8c11d870b99a57b818b4495
Bertrand Juglas <b@juglas.name>
2020-06-20 13:08:07 +0200

    grep added to show differences

--- systemd/ipconfig.sh
+++ systemd/ipconfig.sh
@@ -1,3 +1,5 @@
 IP=`ip address show dev eth0 | grep inet | head -n1 | cut -d ' ' -f 6 | cut -d '/' -f 1`
 echo $IP
+grep 21337 witnet.toml
 sed -i "s/0.0.0.0:21337/$IP:21337/" witnet.toml
+grep $IP witnet.toml
