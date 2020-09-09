eb05827a45522a680d160fb6587eb87679d9c4d8
Bertrand Juglas <bjuglas@crystalchain.io>
2020-09-09 15:43:38 +0200

    add claimExport and fix masterKeyExport

--- docker/compose/bertux-operator-5/claimExport.sh
+++ docker/compose/bertux-operator-5/claimExport.sh
@@ -4,14 +4,29 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 set -u  # considers substituting empty variables as an error
 #set -x
 
+## HELP function
+Usage()
+{
+    echo "Insufficient number of arguments - There must at least WIT_ID"
+		echo "Usage : $0 [WIT_ID]"
+}
+
+if [ "$#" -lt 1 ]
+then
+		Usage
+		exit 1
+fi
+
+## VARIABLES SETTINGS
+WIT_ID=$1
 CONTAINERS=$(docker-compose ps -q)
-EXPORT_DIR="master_keys_$(date +'%Y%m%d%H%M%S')"
+EXPORT_DIR="claim_$(date +'%Y_%m_%d_%H_%M_%S')"
 
 mkdir $EXPORT_DIR
 
 for c in $CONTAINERS; do
     compose_service_name=$(docker inspect $c --format '{{ index .Config.Labels "com.docker.compose.service" }}')
-    docker_key_file=$(docker exec $c witnet node masterKeyExport --write | grep -Po '(Private key written to )\K.*')
+    docker_key_file=$(docker exec $c witnet node claim --identifier $WIT_ID --write | grep -Po '(Signed claiming data written to )\K.*')
     storage_path=$(docker inspect $c --format '{{ range .Mounts }}{{ if eq .Destination "/.witnet" }}{{ .Source }}{{ end }}{{ end }}')
     local_key_file="${storage_path}/${docker_key_file#*/*/}"
     output_dir="${EXPORT_DIR}/${compose_service_name}"
